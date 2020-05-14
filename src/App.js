f58e4e0fb12237f0a6dee879f259a2501d75d8f5
bdiniscia <bdiniscia@gmail.com>
2020-05-14 16:35:52 -0400

    Setting up the styles sheets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import Home from './Components/Home';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Las ArepasconPebre</h1>
+      <Button> Hello</Button>
+      <Home />
     </div>
   );
 }
