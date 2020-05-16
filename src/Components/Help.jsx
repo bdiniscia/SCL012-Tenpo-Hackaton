25ffe7b31392e554a6edf9d4d3f608c1e5d01e1e
bdiniscia <bdiniscia@gmail.com>
2020-05-16 01:15:40 -0400

    Changed the workflow of Help, added a JSON and mapped with a new component

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import './Help.css'
-import SvgProblem from './Widgets/ProblemIcon'
-import SvgNewUsers from './Widgets/NewUserIcon'
-import SvgCardHelp from './Widgets/CardHelpIcon'
-import SvgChargeMoney from './Widgets/ChargeMoneyIcon'
-import SvgWithdrawal from './Widgets/WithdrawIcon'
+import categories from '../help.json'
+import CategoryHelp from './Widgets/CategoryHelp'
 
 const Help = () => {
     return (
@@ -14,48 +11,16 @@ const Help = () => {
                 <input className='searchInput' type="search" placeholder='Busca tu duda aquí' />
             </header>
             <section className='sectionCategories'>
-                <div className='categoryHelp'>
-                    <div className='contentCategory' >
-                        <SvgProblem />
-                        <span className='categoryText'>Tengo un problema</span>
-                    </div>
-                    <img alt='Read more' src={require('../img/next.svg')} />
-                </div>
-                <div className='categoryHelp'>
-                    <div className='contentCategory' >
-                        <SvgNewUsers />
-                        <span className='categoryText'>Soy nuevo en Tenpo</span>
-                    </div>
-                    <img alt='Read more' src={require('../img/next.svg')} />
-                </div>
-                <div className='categoryHelp'>
-                    <div className='contentCategory' >
-                        <SvgCardHelp />
-                        <span className='categoryText'>Tarjeta Tenpo Mastercard</span>
-                    </div>
-                    <img alt='Read more' src={require('../img/next.svg')} />
-                </div>
-                <div className='categoryHelp'>
-                    <div className='contentCategory' >
-                        <SvgChargeMoney />
-                        <span className='categoryText'>Cargar plata a Tenpo</span>
-                    </div>
-                    <img alt='Read more' src={require('../img/next.svg')} />
-                </div>
-                <div className='categoryHelp'>
-                    <div className='contentCategory' >
-                        <SvgWithdrawal />
-                        <span className='categoryText'>Retirar plata</span>
-                    </div>
-                    <img alt='Read more' src={require('../img/next.svg')} />
-                </div>
-                <div className='categoryHelp'>
-                    <div className='contentCategory' >
-                        <SvgWithdrawal />
-                        <span className='categoryText'>Retirar plata</span>
-                    </div>
-                    <img alt='Read more' src={require('../img/next.svg')} />
-                </div>
+                {categories.map(category => {
+                    return(
+                        <CategoryHelp
+                        key={category.id}
+                        icon={category.icon}
+                        title={category.title}
+                        />
+                    )
+                })}
+
             </section>
         </div>
     )
