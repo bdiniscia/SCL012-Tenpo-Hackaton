503c0e9a8875cada975c6fb1d745024d96c9b124
bdiniscia <bdiniscia@gmail.com>
2020-05-15 23:54:54 -0400

    Almost of the styles of the home of Help

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import './Help.css'
+import SvgProblem from './Widgets/ProblemIcon'
+import SvgNewUsers from './Widgets/NewUserIcon'
+import SvgCardHelp from './Widgets/CardHelpIcon'
+import SvgChargeMoney from './Widgets/ChargeMoneyIcon'
+import SvgWithdrawal from './Widgets/WithdrawIcon'
 
 const Help = () => {
     return (
-        <div>
-            <h1>Holis</h1>
+        <div className='help'>
+            <header className='welcomeHelp'>
+                <h1 className='titleHelp'>Centro de ayuda</h1>
+                <input className='searchInput' type="search" placeholder='Busca tu duda aquí' />
+            </header>
+            <section className='sectionCategories'>
+                <div className='categoryHelp'>
+                    <div className='contentCategory' >
+                        <SvgProblem />
+                        <span className='categoryText'>Tengo un problema</span>
+                    </div>
+                    <img alt='Read more' src={require('../img/next.svg')} />
+                </div>
+                <div className='categoryHelp'>
+                    <div className='contentCategory' >
+                        <SvgNewUsers />
+                        <span className='categoryText'>Soy nuevo en Tenpo</span>
+                    </div>
+                    <img alt='Read more' src={require('../img/next.svg')} />
+                </div>
+                <div className='categoryHelp'>
+                    <div className='contentCategory' >
+                        <SvgCardHelp />
+                        <span className='categoryText'>Tarjeta Tenpo Mastercard</span>
+                    </div>
+                    <img alt='Read more' src={require('../img/next.svg')} />
+                </div>
+                <div className='categoryHelp'>
+                    <div className='contentCategory' >
+                        <SvgChargeMoney />
+                        <span className='categoryText'>Cargar plata a Tenpo</span>
+                    </div>
+                    <img alt='Read more' src={require('../img/next.svg')} />
+                </div>
+                <div className='categoryHelp'>
+                    <div className='contentCategory' >
+                        <SvgWithdrawal />
+                        <span className='categoryText'>Retirar plata</span>
+                    </div>
+                    <img alt='Read more' src={require('../img/next.svg')} />
+                </div>
+                <div className='categoryHelp'>
+                    <div className='contentCategory' >
+                        <SvgWithdrawal />
+                        <span className='categoryText'>Retirar plata</span>
+                    </div>
+                    <img alt='Read more' src={require('../img/next.svg')} />
+                </div>
+            </section>
         </div>
     )
 }
---
+++ src/Components/Widgets/backgroundHelp.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function SvgBackgroundHelp(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 360 269" fill="none" {...props}>
+      <path fill="#00BAA3" d="M0 0h360v269H0z" />
+    </svg>
+  );
+}
+
+export default SvgBackgroundHelp;
+
