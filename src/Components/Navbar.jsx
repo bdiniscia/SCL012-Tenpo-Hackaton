2bbc884c475dcd02df24109f8fd655d7584fcdb2
bdiniscia <bdiniscia@gmail.com>
2020-05-15 23:54:54 -0400

    Almost of the styles of the home of Help

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Navbar.css'
+import SvgMoney from './Widgets/SvgMoney'
+import SvgCard from './Widgets/SvgCard'
+import SvgHelp from './Widgets/SvgHelp'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='navbar'>
+            <div className='divIcon'>
+                <SvgMoney className='iconNavbar' />
+                <p className='textIcon'>Tu plata</p>
+            </div>
+            <div className='divIcon'>
+                <SvgCard className='iconNavbar' />
+                <p className='textIcon'>Tarjeta</p>
+            </div>
+            <div className='divIcon'>
+                <SvgHelp className='iconNavbar' />
+                <p className='textIcon'>Ayuda</p>
+            </div>
         </div>
     )
 }
