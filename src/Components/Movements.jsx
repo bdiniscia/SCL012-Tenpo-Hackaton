b277d2e3056d6008fd85d6a103555dd5824707dc
yu-koi <nerymar.guarni@gmail.com>
2020-05-18 11:21:23 -0400

    oredene componente de movements

--- src/Components/Movements.jsx
+++ src/Components/Movements.jsx
@@ -1,121 +1,73 @@
-// import React, { Fragment } from "react";
-// import "./Movements.css";
-// import { Box, Grid, Divider } from "@material-ui/core";
-// import TopbarMovements from "./TopbarMovements";
-// import Typography from "@material-ui/core/Typography";
-// import SquareMenuMovements from "./Widgets/SquareMenuMovements";
+import React, { Fragment } from "react";
+import "./Movements.css";
+import { Box, Grid } from "@material-ui/core";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
+import Typography from "@material-ui/core/Typography";
+import TopbarMovements from "./TopbarMovements";
+import { firebase } from "../firebase";
 
-// import {firebase} from "../firebase"
+const Prueba = () => {
+  const [movement, setMovement] = React.useState([]);
 
+  React.useEffect(() => {
+    const obtenerDatos = async () => {
+      try {
+        const db = firebase.firestore();
+        const data = await db.collection("Movements").get();
+        const arrayData = data.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(arrayData);
+        setMovement(arrayData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerDatos();
+  }, []);
 
+  return (
+    <Fragment>
+      <TopbarMovements />
+      <Grid container className="topbar">
+        <Grid item xs={12} sm={12} md={12}>
+          <Box border={0} p={1} bgcolor="#F2F2F2">
+            <Typography>
+              <h2>
+                <b>Transsacciones descontadas</b>
+              </h2>
+            </Typography>
+          </Box>
+        </Grid>
 
-// const Movements = () => {
+        <Grid item xs={12} sm={12} md={12}>
+          {movement.map((item) => (
+            <Grid item xs={12} sm={12} md={12}>
+              <Box p={0} mt={0.2} bgcolor="#fff">
+                {
+                  <div key={item.id}>
+                    <p>
+                      <b>
+                        {item.transaccion} {item.monto}
+                      </b>
+                    </p>
 
-//   const [movement, setMovement] = React.useState([])
+                    <p>
+                      {item.estado} {item.fecha}{" "}
+                      <span>
+                        <ErrorOutlineIcon />
+                      </span>
+                    </p>
+                  </div>
+                }
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
+};
 
-//   React.useEffect(() => {
-
-//     const obtenerDatos = async () => {
-
-//       try {
-//         const db = firebase.firestore()
-//         const data = await db.collection("Movements").get()
-//         const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data()}))
-//         console.log(arrayData)
-//         setMovement(arrayData)
-        
-//       } catch (error) {
-//         console.log(error)
-        
-//       }
-
-//     }
-//     obtenerDatos()
-
-//   }, [])
-
-
-//   return (
-
-//     <Fragment>
-//      <TopbarMovements />
-
-    
-
-//     <div className="container mt-3">
-//       <div className="row">
-//        <div className="cold-md-6">
-//         <ul>
-//           {
-//             movement.map(item => (
-//             <li key={item.id}>
-//               <p>{item.transaccion} {item.monto}</p>
-//               <p>{item.estado} {item.fecha}</p>
-              
-              
-//             </li>
-//             ))
-//           }
-//         </ul>
-//        </div>
-
-//       </div>
-//     </div>
-
-//     </Fragment>
-
-
-    // <Fragment>
-    
-
-    //   <Grid container className="topbar">
-    //     <Grid item xs={12} sm={12} md={12}>
-    //       <Box border={2} p={4} bgcolor="#F2F2F2">
-    //         <Typography variant="p">Transsacciones descontadas</Typography>
-    //       </Box>
-    //     </Grid>
-
-    //     <Grid item xs={12} sm={6} md={12}>
-    //       <Box border={2} mt={0.3} p={2}>
-    //         BOX DO
-    //         <SquareMenuMovements className="squareMenu" bgcolor="" />
-    //       </Box>
-    //     </Grid>
-
-    //     <Grid item xs={12} sm={6} md={12}>
-    //       <Box border={2} mt={0.3} p={2}>
-    //         BOX DO
-    //         <SquareMenuMovements className="squareMenu" />
-    //       </Box>
-    //     </Grid>
-
-    //     <Grid item xs={12} sm={6} md={12}>
-    //       <Box border={2} mt={0.3} p={2}>
-    //         BOX DO
-    //         <SquareMenuMovements className="squareMenu" />
-    //       </Box>
-    //     </Grid>
-
-    //     <Grid item xs={12} sm={6} md={12}>
-    //       <Box border={2} mt={0.3} p={2}>
-    //         BOX DO
-    //         <SquareMenuMovements className="squareMenu" />
-    //       </Box>
-    //     </Grid>
-
-    //     <Grid item xs={12} sm={6} md={12}>
-    //       <Box border={2} mt={0.3} p={2}>
-    //         BOX DO
-    //         <SquareMenuMovements className="squareMenu" />
-    //       </Box>
-    //     </Grid>
-
-    //     <Divider />
-    //   </Grid>
-    // </Fragment>
-
-
-//   );
-// };
-
-// export default Movements;
+export default Prueba;
--- src/Components/TopbarMovements.jsx
+++ src/Components/TopbarMovements.jsx
@@ -1,33 +1,29 @@
 import React from "react";
 import "./TopbarMovements.css";
 import SquareMenuMovements from "./Widgets/SquareMenuMovements";
-import Back from './Widgets/Back'
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, IconButton } from "@material-ui/core";
-
+import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 
 const useStyles = makeStyles((theme) => ({
   back: {
     flexGrow: 1,
     marginRight: theme.spacing(2),
   },
-
 }));
 
 const NavbarMovements = () => {
   const classes = useStyles();
 
-
   return (
     <div className="topbar">
       <IconButton className={classes.title}>
-        <Back />
+        <KeyboardBackspaceIcon />
       </IconButton>
-     
+
       <Typography variant="p">TU PLATA DISPONIBLE</Typography>
-      {/* <Typography variant="p">$100.000</Typography> */}
+      <Typography variant="p"><b>$100.000</b></Typography>
       <SquareMenuMovements className="squareMenu" />
-     
     </div>
   );
 };
