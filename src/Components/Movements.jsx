fefc5c1c6215d87336a4b0afd9daa7a3a92ec2ad
yu-koi <56682591+Yu-Koi@users.noreply.github.com>
2020-05-19 09:09:39 -0400

    agregue funcionalidad de guardar datos en firebase a compoente Support (#15)
    * agregue componente de movements
    
    * agregue componente movements, archivo .gitignore, topbarMovements y inicializacion de firebase
    
    * instale firebase
    
    * mergeando
    
    * Remove the now ignored directory node_modules
    
    * oredene componente de movements
    
    * cambie nombre classes en componentes movements y topbar
    
    * cambie importacacion del topbarMovements desde App.js
    
    * agregue cambios de estilos de clases para css de movements
    
    * modifique en columna la plata disponible
    
    * aliniacion de boton tooltip y texto en movimientos
    
    * cambie nombre de clase transtationState
    
    * agregue funcionalidad de guardar datos en firebase al componente Support
    
    Co-authored-by: Bárbara Di Niscia <bdiniscia@gmail.com>

--- src/Components/Movements.jsx
+++ src/Components/Movements.jsx
@@ -5,14 +5,19 @@ import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 import Typography from "@material-ui/core/Typography";
 import { firebase } from "../firebase";
 
+
 const useStyles = makeStyles((theme) => ({
   transtationState: {
     marginLeft: theme.spacing(2),
   },
+
 }));
+
+
 
 const Prueba = () => {
   const classes = useStyles();
+  
 
   const [movement, setMovement] = React.useState([]);
 
--- src/Components/Widgets/InputRequirement.jsx
+++ src/Components/Widgets/InputRequirement.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import './InputRequirement.css'
-import Button from './Button'
+import React from "react";
+import { firebase } from "../../firebase";
+import "./InputRequirement.css";
+import Button from "./Button";
 
 const InputRequirement = () => {
-    return (
-        <div className='inputRequirement' >
-            <p className='question' >Cuéntanos qué pasó y solucionaremos tu requerimiento:</p>
-            <textarea className='textareaRequirement' placeholder="Escribe aquí..." />
-            <div className='divButton'>
-                <Button title='Enviar' />
-            </div>
-        </div>
-    )
-}
+  const [requirements, setRequirement] = React.useState("");
 
-export default InputRequirement
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    console.log(requirements);
+
+    if (!requirements.trim()) {
+      console.log("esta vacio");
+      return;
+    }
+    try {
+      const db = firebase.firestore();
+      const newRequirement = {
+        message: requirements,
+        fecha: Date.now(),
+      };
+      const data = await db.collection("requirements").add(newRequirement);
+
+      setRequirement([...requirements, { ...newRequirement, id: data.id }]);
+      setRequirement("");
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(requirements);
+  };
+
+  return (
+    <div className="inputRequirement">
+      <p className="question">
+        Cuéntanos qué pasó y solucionaremos tu requerimiento:
+      </p>
+      <div className="pruebaa">
+        <form onSubmit={sendMessage}>
+          <input
+            type="text"
+            className="textareaRequirement"
+            placeholder="Escribe aquí..."
+            onChange={(e) => setRequirement(e.target.value)}
+            value={requirements}
+          />
+
+          <Button type="submit" title="Enviar" ENVIAR />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default InputRequirement;
