3df703dedaff5330cd671d19fbe7023be0bb628b
bdiniscia <bdiniscia@gmail.com>
2020-05-19 07:44:29 -0400

    Done the styles in Home. Added the Link to Movements

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react'
 import './Home.css'
 import Topbar from './Topbar'
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
@@ -11,7 +12,9 @@ const Home = () => {
                 <header className='headerHome' >
                     <span className='previewAmount'>TU PLATA DISPONIBLE</span>
                     <h1 className='amountHome'>$100.000</h1>
-                    <span className='toDiscount'>Monto a descontar: $50.000   &gt;</span>
+                    <Link to='/Movements' style={{textAlign: 'center', textDecoration: 'none', color: '#fff'}}>
+                        <span className='toDiscount'>Monto a descontar: $50.000   &gt;</span>
+                    </Link>
                 </header>
                 <nav className='menuHome'>
                     <div className='optionsMenuHome borderRight'>
@@ -27,6 +30,7 @@ const Home = () => {
                         <span className='textOptionMenu'>Historial</span>
                     </div>
                 </nav>
+                <h5 className='titleSectionHome'>Más servicios para ti</h5>
                 <section className='moreOptionsSection' >
                     <div className='optionsMore'>
                         <img className='iconOptionsMore' src={require('../img/01payment.svg')} alt='Pago de cuentas' />
@@ -41,11 +45,11 @@ const Home = () => {
                         <span className='textOptionsMore'>Cobrar y pagar</span>
                     </div>
                     <div className='optionsMore'>
-                        <img className='iconOptionsMore' src={require('../img/04paypal.svg')} alt='Paypal' />
+                        <img className='iconOptionsMore smallerIconsHome' src={require('../img/04paypal.svg')} alt='Paypal' />
                         <span className='textOptionsMore'>Paypal</span>
                     </div>
                     <div className='optionsMore'>
-                        <img className='iconOptionsMore' src={require('../img/05pocket.svg')} alt='Bolsillo' />
+                        <img className='iconOptionsMore smallerIconsHome' src={require('../img/05pocket.svg')} alt='Bolsillo' />
                         <span className='textOptionsMore'>Bolsillo</span>
                     </div>
                     <div className='optionsMore'>
